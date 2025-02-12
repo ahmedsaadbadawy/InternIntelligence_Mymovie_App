cff298d4744e49299b809a6abf46b09f545bc254
ahmedsaadbadawy <ahmedzack168@gmail.com>
2025-02-12 06:32:46 +0200

    Test the movie Repository.

--- lib/data/models/movies_result_model.dart
+++ lib/data/models/movies_result_model.dart
@@ -27,7 +27,7 @@ class MoviesResultModel {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = Map<String, dynamic>();
+    final Map<String, dynamic> data = <String, dynamic>{};
     //data['page'] = page;
     if (movies != null) {
       data['results'] = movies!.map((v) => v.toJson()).toList();
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'Core/errors/app_error.dart';
+import 'Core/usecases/no_params.dart';
 import 'data/datasources/api_client.dart';
 import 'data/datasources/movie_remote_data_source.dart';
+import 'data/repositories/movie_repository_impl.dart';
+import 'domain/entities/movie_entity.dart';
+import 'domain/repositories/movie_repository.dart';
+import 'domain/usecases/get_trending.dart';
 import 'presentaion/views/homa_screen.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  print('API_KEY: ${dotenv.env['API_KEY']}');
+  //print('API_KEY: ${dotenv.env['API_KEY']}');
   ApiClient apiClient = ApiClient(Dio());
   MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  dataSource.getRecommendations(845781);
+  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
+  GetTrending getTrending = GetTrending(movieRepository);
+  final Either<AppError, List<MovieEntity>> eitherResponse =
+      await getTrending(NoParams());
+  eitherResponse.fold(
+    (l) {
+      print('error');
+      print(l);
+    },
+    (r) {
+      print('list of movies');
+      print(r);
+    },
+  );
   runApp(const MyApp());
 }
 
