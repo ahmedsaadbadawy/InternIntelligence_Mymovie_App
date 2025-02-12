0f86df02eea7f732f126a692e749ce2cfa17983f
ahmedsaadbadawy <ahmedzack168@gmail.com>
2025-02-12 06:56:28 +0200

    - Add Dependency Injection file. - Add get_it package. - Test the DI.

---
+++ lib/di/get_it.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import '../data/datasources/api_client.dart';
+import '../data/datasources/movie_remote_data_source.dart';
+import '../data/repositories/movie_repository_impl.dart';
+import '../domain/repositories/movie_repository.dart';
+import '../domain/usecases/get_coming_soon.dart';
+import '../domain/usecases/get_playing_now.dart';
+import '../domain/usecases/get_popular.dart';
+import '../domain/usecases/get_trending.dart';
+
+final getItInstance = GetIt.I;
+Future init() async {
+  
+  getItInstance.registerLazySingleton<Dio>(() => Dio());
+
+  getItInstance
+      .registerLazySingleton<ApiClient>(() => ApiClient(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetTrending>(() => GetTrending(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetPopular>(() => GetPopular(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetPlayingNow>(
+      () => GetPlayingNow(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetComingSoon>(
+      () => GetComingSoon(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRepository>(
+      () => MovieRepositoryImpl(getItInstance()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
+import 'dart:async';
+
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'di/get_it.dart' as getit;
 import 'Core/errors/app_error.dart';
 import 'domain/usecases/no_params.dart';
-import 'data/datasources/api_client.dart';
-import 'data/datasources/movie_remote_data_source.dart';
-import 'data/repositories/movie_repository_impl.dart';
 import 'domain/entities/movie_entity.dart';
-import 'domain/repositories/movie_repository.dart';
 import 'domain/usecases/get_trending.dart';
 import 'presentaion/views/homa_screen.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   //print('API_KEY: ${dotenv.env['API_KEY']}');
-  ApiClient apiClient = ApiClient(Dio());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+  unawaited(getit.init());
+  GetTrending getTrending = getit.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold(
