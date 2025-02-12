995c14d1f310799911d5bfe8b601a73f3611b99f
ahmedsaadbadawy <ahmedzack168@gmail.com>
2025-02-12 07:21:13 +0200

    - Add flutter_screenutil Package. - Add ScreenUtilInit in main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,30 @@
 import 'dart:async';
 
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'di/get_it.dart' as getit;
-import 'Core/errors/app_error.dart';
-import 'domain/usecases/no_params.dart';
-import 'domain/entities/movie_entity.dart';
-import 'domain/usecases/get_trending.dart';
 import 'presentaion/views/homa_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  //print('API_KEY: ${dotenv.env['API_KEY']}');
+  // print('API_KEY: ${dotenv.env['API_KEY']}');
   unawaited(getit.init());
-  GetTrending getTrending = getit.getItInstance<GetTrending>();
-  final Either<AppError, List<MovieEntity>> eitherResponse =
-      await getTrending(NoParams());
-  eitherResponse.fold(
-    (l) {
-      print('error');
-      print(l);
-    },
-    (r) {
-      print('list of movies');
-      print(r);
-    },
-  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: HomeScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(414, 896),
+      builder: (context, child) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
--- lib/presentaion/views/homa_screen.dart
+++ lib/presentaion/views/homa_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Home Screen',
+          style: TextStyle(fontSize: 14.sp),
+        ),
+      ),
+    );
   }
 }
