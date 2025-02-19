a7665cee988814c8ec35e1ab3bc5da13091231af
ahmedsaadbadawy <ahmedzack168@gmail.com>
2025-02-19 19:35:19 +0200

    Add search screen and update app router.

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -3,13 +3,15 @@ import 'package:go_router/go_router.dart';
 import 'package:movie_app/presentaion/views/home/homa_screen.dart';
 import 'package:movie_app/presentaion/views/movie_detail/manager/MovieDetail/movie_detail_cubit.dart';
 import 'package:movie_app/presentaion/views/movie_detail/movie_detail_screen.dart';
+import 'package:movie_app/presentaion/views/search/manager/SearchMovie/search_movie_cubit.dart';
+import 'package:movie_app/presentaion/views/search/search_screen.dart';
 
 import '../../di/get_it.dart';
 
 abstract class AppRouter {
   static const kHomeScreen = '/HomeScreen';
   static const kMovieDetailScreen = '/MovieDetailScreen';
-  static const kSearchView = '/SearchView';
+  static const kSearchScreen = '/SearchScreen';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -23,6 +25,16 @@ abstract class AppRouter {
             ..loadMovieDetail(state.extra as int),
           child: MovieDetailScreen(
             movieId: state.extra as int,
+          ),
+        ),
+      ),
+      GoRoute(
+        path: kSearchScreen,
+        builder: (context, state) => BlocProvider(
+          create: (context) => getItInstance<SearchMoviesCubit>()
+            ..searchForMovies(state.extra as String),
+          child: const SearchScreen(
+            //searchText: state.extra as String,
           ),
         ),
       ),
